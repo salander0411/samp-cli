7d687759317451af8b3bb55398df7244bc33b167
salander0411 <salander0411@gmail.com>
2023-10-09 20:40:11 +0800

    revise APIs

--- src/commands/generate/generate.js
+++ src/commands/generate/generate.js
@@ -1,24 +1,23 @@
 const inputUtil = require("../../shared/inputUtil");
 const parser = require("../../shared/parser");
-const { Configuration, OpenAIApi } = require("openai");
 const settingsUtil = require("../../shared/settingsUtil");
 const fs = require("fs-extra");
 var Spinner = require('cli-spinner').Spinner;
 const baseFile = require("../../shared/baseFile.json");
-var spinner = new Spinner('Waiting for ChatGPT... %s');
+const JSON= require("JSON");
+var util= require('util');
+const utf8Decoder = new util.TextDecoder("utf-8", { ignoreBOM: true });
+
+var spinner = new Spinner('Waiting for Claude... %s');
 spinner.setSpinnerString('|/-\\');
+
+const { BedrockRuntimeClient, InvokeModelCommand } = require("@aws-sdk/client-bedrock-runtime");
+
 async function run(cmd) {
-  console.log("*** Note: This is an experimental feature and depends on the ChatGPT API. Make sure you review the output carefully before using it in production ***")
+  console.log("*** Note: This is an experimental feature and depends on the BedRock Claude v1.3 API. Make sure you review the output carefully before using it in production ***")
   const output = cmd.output.toLowerCase();
   if (output !== "sam" && !cmd.outputFile) {
     console.log(`You need to specify an output file with --output-file`);
-    return;
-  }
-  const apiKey = settingsUtil.getConfigSource().openaiApiKey;
-  if (!apiKey) {
-    console.log(
-      "You need to set your OpenAI API key. Use the 'samp configure' command to do this. You can get your API key from https://platform.openai.com/account/api-keys"
-    );
     return;
   }
 
@@ -40,10 +39,6 @@ async function run(cmd) {
     ownTemplate.Resources = ownTemplate.Resources || {};
   }
 
-  const configuration = new Configuration({
-    apiKey,
-  });
-  const openai = new OpenAIApi(configuration);
   let outputString;
   let language;
   switch (output.substring(0, 3)) {
@@ -70,31 +65,51 @@ async function run(cmd) {
   }
   const prompt = `Generate this in AWS ${outputString}: ${cmd.query}. Only return code.`;
 
-  const openAiRequest = {
-    model: cmd.model,
-    messages: [
-      {
-        role: "user",
-        content: prompt,
+  const claudeBody = JSON.stringify({
+        "prompt": "\n\nHuman:" + prompt +"\n\nAssistant:",
+        "temperature": 0.5,
+        "top_p": 1,
+        "top_k": 250,
+        "max_tokens_to_sample": 400,
+        "stop_sequences": ["\n\nHuman:"]
       }
-    ],
-    temperature: 0.5,
-    max_tokens: 2000
+  )
+
+  const claudeRequest = {
+    body: claudeBody,
+    modelId: 'anthropic.claude-v1',
+    accept: 'application/json',
+    contentType: 'application/json'
   };
+
   spinner.start();
-  const response = await openai.createChatCompletion(openAiRequest);
+
+  //call bedrock API to generate response
+  const bedrockclient = new BedrockRuntimeClient({
+    region: "us-east-1",
+    profile: "global"
+  });
+
+  const command = new InvokeModelCommand(claudeRequest);
+  const response = await bedrockclient.send(command);
+
   spinner.stop();
-  let text = response.data.choices[0].message.content;
+
+  let text = JSON.parse(utf8Decoder.decode(response.body)).completion;
+
+  //console.log(typeof text); // string
+
   // get the first JSON object in the text
   let obj;
   if (output === "sam") {
     try {
       obj = JSON.parse(text.replace(/\n/g, '').replace(/```/g, '').match(/{.*}/)[0]);
+      console.log("output is sam, obj is", obj);
     } catch (e) {
       try {
         obj = parser.parse("yaml", text);
       } catch (e) {
-        console.log(`Couldn't parse the output from ChatGPT. Try again. The output was: \n${text}\n\nThe error was: ${e}`);
+        console.log(`Couldn't parse the output from Claude. Try again. The output was: \n${text}\n\nThe error was: ${e}`);
         return;
       }
     }
@@ -104,7 +119,7 @@ async function run(cmd) {
 
     console.log(`\n\nGenerated the following resources:\n\n${parser.stringify("yaml", obj)}`);
   } else {
-    console.log(text);
+    console.log("output not sam, text is ",text);
     //check if text has a row starting with ``` followed by text. If so, remove that text
     const match = text.match(/```.+\n/);
     if (match && match[0]) {
